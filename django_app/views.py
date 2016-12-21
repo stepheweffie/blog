0fcefa7f49789dc547909ae73ed1c38c20667516
stepheweffie <stephie.maths@gmail.com>
2016-12-21 16:04:15 -0500

    fixed published time and date

--- django_app/views.py
+++ django_app/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
+from django.utils import timezone
 from django_app.models import Post
-
-
 
 # Create your views here.
 def post_list(request):
-    return render(request, 'post_list.html',)
-
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    print('---- posts ----')
+    print(posts)
+    print(Post.objects.all())
+    print(timezone.now())
+    return render(request, 'post_list.html', {'posts': posts})
